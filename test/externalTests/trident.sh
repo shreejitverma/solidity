5b823b42d41109460fb8ef241e3d2b15e0da5218
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-10-27 12:22:17 +0200

    External test for sushiswap/trident

--- test/externalTests/trident.sh
+++ test/externalTests/trident.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with solidity.  If not, see <http://www.gnu.org/licenses/>
 #
-# (c) 2019 solidity contributors.
+# (c) 2021 solidity contributors.
 #------------------------------------------------------------------------------
 
 set -e
@@ -27,16 +27,26 @@ source test/externalTests/common.sh
 verify_input "$1"
 SOLJSON="$1"
 
-function compile_fn { npm run build; }
-function test_fn { npm test; }
+function compile_fn { yarn build; }
 
-function gnosis_safe_test
+function test_fn {
+    # shellcheck disable=SC2046
+    TS_NODE_TRANSPILE_ONLY=1 npx hardhat test --no-compile $(
+        # TODO: We need to skip Migration.test.ts because it fails and makes other tests fail too.
+        # Replace this with `yarn test` once https://github.com/sushiswap/trident/issues/283 is fixed.
+        find test/ -name "*.test.ts" ! -path "test/Migration.test.ts"
+    )
+}
+
+function trident_test
 {
-    local repo="https://github.com/gnosis/safe-contracts.git"
-    local branch=main
+    # TODO: Switch to https://github.com/sushiswap/trident / master once
+    # https://github.com/sushiswap/trident/pull/282 gets merged.
+    local repo="https://github.com/solidity-external-tests/trident.git"
+    local branch=master_080
     local config_file="hardhat.config.ts"
-    local config_var=userConfig
-    local min_optimizer_level=2
+    local config_var=config
+    local min_optimizer_level=1
     local max_optimizer_level=3
 
     local selected_optimizer_levels
@@ -46,22 +56,25 @@ function gnosis_safe_test
     setup_solcjs "$DIR" "$SOLJSON"
     download_project "$repo" "$branch" "$DIR"
 
-    # NOTE: The patterns below intentionally have hard-coded versions.
-    # When the upstream updates them, there's a chance we can just remove the regex.
-    sed -i 's|"@gnosis\.pm/mock-contract": "\^4\.0\.0"|"@gnosis.pm/mock-contract": "github:solidity-external-tests/mock-contract#master_080"|g' package.json
-    sed -i 's|"@openzeppelin/contracts": "\^3\.4\.0"|"@openzeppelin/contracts": "^4.0.0"|g' package.json
+    # TODO: Currently tests work only with the exact versions from yarn.lock.
+    # Re-enable this when https://github.com/sushiswap/trident/issues/284 is fixed.
+    #neutralize_package_lock
 
-    neutralize_package_lock
     neutralize_package_json_hooks
     force_hardhat_compiler_binary "$config_file" "$SOLJSON"
     force_hardhat_compiler_settings "$config_file" "$min_optimizer_level" "$config_var"
-    npm install
+    yarn install
 
     replace_version_pragmas
+    force_solc_modules "${DIR}/solc"
+
+    # @sushiswap/core package contains contracts that get built with 0.6.12 and fail our compiler
+    # version check. It's not used by tests so we can remove it.
+    rm -r node_modules/@sushiswap/core/
 
     for level in $selected_optimizer_levels; do
         hardhat_run_test "$config_file" "$level" compile_fn test_fn "$config_var"
     done
 }
 
-external_test Gnosis-Safe gnosis_safe_test
+external_test Trident trident_test
